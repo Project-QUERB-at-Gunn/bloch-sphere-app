7258cb63e31a246c4aa6b4d6aa3d25da56f79bd5
mg27713 <68036487+mg27713@users.noreply.github.com>
2021-02-16 17:14:26 -0800

    Update main.js

--- main.js
+++ main.js
@@ -2,21 +2,24 @@ import * as mat4 from "./glmatrix/mat4.js";
 
 const vsSource = `
     attribute vec4 aVertexPosition;
+    uniform vec4 bVertexPosition;
     
     uniform mat4 uModelViewMatrix;
     uniform mat4 uProjectionMatrix;
     
     void main() {
-        origPosition = aVertexPosition;
+        bVertexPosition = aVertexPosition;
         gl_Position = uProjectionMatrix * uModelViewMatrix * aVertexPosition;
     }
 `;
 
 const fsSource = `
+    uniform vec4 bVertexPosition;
+    
     uniform vec4 origin;
     
     void main() {
-        gl_FragColor = (aVertexPosition == origin) ? vec4(1.0, 1.0, 0.0, 1.0) : vec4(0.5, 0.5, 0.0, 1.0);
+        gl_FragColor = (bVertexPosition == origin) ? vec4(1.0, 1.0, 0.0, 1.0) : vec4(0.5, 0.5, 0.0, 1.0);
     }
 `;
 
