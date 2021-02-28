fcdee37655d5423e4b281234b3d12f227191ade3
mg27713 <68036487+mg27713@users.noreply.github.com>
2021-02-27 21:50:32 -0800

    more

--- main.js
+++ main.js
@@ -190,19 +190,19 @@ function drawFrame() {
     
     const rad2 = 0.05;
     const vert2 = Math.floor(rad2*300 + 5);
-    let pos2 = [pointposition.x*radius, pointposition.y*radius, pointposition.z-1.0, 1.0];
+    let pos2 = [pointposition.x, pointposition.y, pointposition.z-1.0, 1.0];
     
     for (var i = 0.0; i < Math.PI*2; i += Math.PI*2/(vertexCount)) {
-        pos2.push(Math.cos(i)*rad2 + pointposition.x*radius); // x
-        pos2.push(Math.sin(i)*rad2 + pointposition.y*radius); // y
+        pos2.push(Math.cos(i)*rad2 + pointposition.x); // x
+        pos2.push(Math.sin(i)*rad2 + pointposition.y); // y
 //         positions.push(Math.cos(i+Math.PI*2/vertexCount)*radius); positions.push(Math.sin(i+Math.PI*2/vertexCount)*radius);
         
         pos2.push(pointposition.z-1.0); // z=0
         pos2.push(1.0); // w=1
     }
     
-    pos2.push(pointposition.x*radius+rad2);
-    pos2.push(pointposition.y*radius);
+    pos2.push(pointposition.x+rad2);
+    pos2.push(pointposition.y);
     pos2.push(pointposition.z-1.0);
     pos2.push(1.0);
     
